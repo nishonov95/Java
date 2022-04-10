e4ce43f24d33e37e3e78665c21461c9f0df3f0a5
Xolmurod <softrishton@gmail.com>
2022-04-10 17:04:25 +0500

    massiv va objectlar qoshildi

--- script/script.js
+++ script/script.js
@@ -26,7 +26,7 @@
 
 
 //Funksiyalar
-"use strict"
+
 // function gettext(){
 //     console.log("hello world");
 // }
@@ -47,18 +47,86 @@
 // console.log(num);
 
 
-function playFootbol(player, callback){
-    console.log(`Name of player ${player}`)
-    callback()
-}
+// function playFootbol(player, callback){
+//     console.log(`Name of player ${player}`)
+//     callback()
+// }
 
-playFootbol("MoSalah", function(){
-    console.log("his shot 100 gools")
-})
+// playFootbol("MoSalah", function(){
+//     console.log("his shot 100 gools")
+// })
 
 
 
-let text = "hello world"
+// let text = "hello world"
 
-console.log(text.toUpperCase());
-console.log(text.indexOf("d"));
+// console.log(text.toUpperCase());
+// console.log(text.indexOf("d"));
+
+
+
+// const person = {
+//     name: `Xolmurod`,
+//     lastname: `Nishonov`,
+//     age: 25,
+//     wishes: [],
+//     language: function(){},
+//     mather:{
+//         name: `Jumagul`,
+//         lastname:`Ulmasova`
+//     },
+// }
+
+// console.log(Object.keys(person).length);
+
+// for (let key in person){
+//     if (typeof person[key]=="object"){
+//         for (let i in person[key]){
+//             console.log(`xususiyati ${key}  miqdori ${person[key][i]}`);
+//         }
+//     } else{
+//         console.log(`xususiyati ${key}  miqdori ${person[key]}`);
+//     }    
+// }
+// let i = 0
+// for (let key in person){
+// i++
+// }
+// console.log(i);
+
+
+
+// let myFirstMethod={
+// makeExempla: function(){
+//     console.log(`salom`);
+// }
+// }
+// myFirstMethod.makeExempla()
+
+
+"use strict"
+const arr = [1,2,3,4]
+//masivga qo`shish va olib tashlash
+//arr.pop() => masivni oxiridan olib tashlaydi.
+//arr.push(6) => massivni oxiriga qo`shish.
+//arr.unshift(0) => massivni boshiga element qo`shadi.
+//arr.shift()   => massivni boshidan olib tashlaydi.
+
+//console.log(arr);
+
+//-------massiv elementlarini alohida olish
+//for (let i=0; i<arr.length; i++){
+//console.log(arr[i]);
+//}
+
+// arr.forEach(function(item, index, arr){
+//     console.log(arr);
+// })
+
+
+
+let questions = prompt('','')
+
+let answers = questions.split('','')
+
+console.log(answers);
